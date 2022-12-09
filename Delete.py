17778a27a175142300b50b7c2deaf21f288f315b
Leopard527 <115495298+Leopard527@users.noreply.github.com>
2022-12-09 07:51:46 +0100

    Delete print functie

--- Delete.py
+++ Delete.py
@@ -6,7 +6,6 @@ def Delete(ClientInput: list, Anser: str, DeletePlace: int) -> list:
 
     if DeletePlace <= 1: 
         ClientInput.insert(0, Anser)
-        print(ClientInput, Anser, DeletePlace, 'test1a')
         return ClientInput
 
     if len(ClientInput) < DeletePlace:
@@ -16,7 +15,6 @@ def Delete(ClientInput: list, Anser: str, DeletePlace: int) -> list:
     for place, txt in enumerate(ClientInput):
         if txt in OPERATOR and ClientInput[place-1] in OPERATOR:
             ClientInput.insert(place, Anser)
-            print(ClientInput, Anser, DeletePlace, 'test1b')
             break
     return ClientInput
 
