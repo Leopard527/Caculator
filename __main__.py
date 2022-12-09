5b8cd9bdd7eb80fc3ede2a863c544ae57f90258c
Leopard527 <115495298+Leopard527@users.noreply.github.com>
2022-12-09 07:53:18 +0100

    Garbage

--- __main__.py
+++
@@ -1,7 +0,0 @@
-from UserInput import UserInput
-from Sorting import Sorting
-
-ClientInput = UserInput.UserInput() 
-CheckHaakjes = Sorting.Haakjes(ClientInput)
-if CheckHaakjes is None:
-    print('No haakjes')
