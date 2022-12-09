8fedeb7a20c3f48c28bba925aecd305613d1de88
Leopard527 <115495298+Leopard527@users.noreply.github.com>
2022-12-09 07:53:04 +0100

    Userinput has a security

--- UserInput.py
+++ UserInput.py
@@ -1,4 +1,16 @@
+from Calculate import OPERATOR_TIMES, OPERATOR_DEFICION, OPERATOR_ADD, OPERATOR_SUBTRACT
+from string import digits
+
+StringReplaeInt = {'1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '0':0}
 class UserInput:
     def UserInput() -> list:
-        ClientInput = input(': ')
-        return list(ClientInput)
+        ClientInput = input('Please enter youre som: ')
+        while ClientInput not in (digits) and ClientInput in ('+-()*x/\\'):
+            ClientInput = input('Please enter youre som: ')
+        ClientInput = list(ClientInput)
+        #Credits https://stackoverflow.com/users/8747/rob%e1%b5%a9
+        [StringReplaeInt.get(item, item) for item in ClientInput]
+        return ClientInput
+
+
+        
